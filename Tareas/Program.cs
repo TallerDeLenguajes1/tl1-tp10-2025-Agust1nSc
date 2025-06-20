ff5c7f7ec26abf052d1096e12a4481aa1884dc1b
“Agust1nSc” <agustinsaccone9@gmail.com>
2025-06-20 17:47:18 -0300

    Tareas, titulo y estado

---
+++ Tareas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using EspacioTareas;
+
+HttpClient client = new HttpClient();
+
+string url = "https://jsonplaceholder.typicode.com/todos/";
+
+string cuerpo = await client.GetStringAsync(url);
+
+List<Tarea> tareas = JsonSerializer.Deserialize<List<Tarea>>(cuerpo);
+
+foreach (var tareaAux in tareas.Where(tareaAux => !tareaAux.completed))
+{
+    Console.WriteLine("Tarea:" + tareaAux.title + "Estado: Pendiente");
+}
+
+foreach (var tareaAux in tareas.Where(tareaAux => tareaAux.completed))
+{
+    Console.WriteLine("Tarea:" + tareaAux.title + "Estado: Realizada");
+}
