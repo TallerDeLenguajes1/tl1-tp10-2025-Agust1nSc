1942cd389ec27ed388aeab7342970274d5a03dee
“Agust1nSc” <agustinsaccone9@gmail.com>
2025-06-20 19:45:13 -0300

    Ejercicio 2 completo

---
+++ Usuarios/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using EspacioUsuarios;
+
+HttpClient client = new HttpClient();
+
+string url = "https://jsonplaceholder.typicode.com/users";
+
+string cuerpo = await client.GetStringAsync(url);
+
+List<Usuario> usuarios = JsonSerializer.Deserialize<List<Usuario>>(cuerpo);
+
+Console.WriteLine("Primeros 5 usuarios");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Usuario: " + usuarios[i].name);
+    Console.WriteLine("Correo: " + usuarios[i].email);
+    Console.WriteLine("Domicilio: "+ usuarios[i].address.city  + " "+ usuarios[i].address.street +" "+ usuarios[i].address.suite);
+}
